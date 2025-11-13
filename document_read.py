50e0e9667534e414037cca30b8440e8a5e50f2c1
Fonzzy1 <alfiechadwick@hotmail.com>
2025-11-13 12:11:17 +1100

    Final fixes before workshop

--- document_read.py
+++ document_read.py
@@ -1,6 +1,22 @@
-# Import the query_llm function from our hyperspecific.py file!
-# This allows us to reuse functions that we have allready written
-from hyperspecific import query_llm
+import ollama
+
+
+# Write out the query llm function again
+def query_llm(prompt, model="qwen2.5:0.5b"):
+    """
+    Given a 'prompt' and the name of a model,
+    return the LLM's text response (uses ollama SDK).
+    Because the model has a default, we don't need to be explicit in which model
+    to use if you don't want to.
+    """
+    # Send the request to Ollama and get the response dictionary (a kind of
+    # "named list").
+    response = ollama.chat(
+        model=model, messages=[{"role": "user", "content": prompt}]
+    )
+    # Return ONLY the LLM's textual answer from the response.
+    return response.message.content
+
 
 # Import PyPDF2 for PDF reading
 import PyPDF2
@@ -29,9 +45,7 @@ pdf_text = read_pdf_text(pdf_path)
 
 # Create a prompt that includes the PDF's contents
 # You can stick strings together with + symbols to make it one big string
-prompt = (
-    pdf_text + "\n\n" + "What is the name of the author of this document"
-)
+prompt = pdf_text + "\n\n" + "What is the name of the author of this document"
 
 # Use query_llm (imported from hyperspecific.py) to get the LLM's answer!
 llm_answer = query_llm(prompt)
